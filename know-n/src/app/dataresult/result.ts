6d29881239bd85e1a6640b44bdf95c4a3c5857ca
JMarzipan <jwmarzloff@gmail.com>
2018-02-12 21:29:54 -0500

    Added simple data class for testing.

--- know-n/src/app/dataresult/result.ts
+++ know-n/src/app/dataresult/result.ts
@@ -1,4 +1,11 @@
-public class schoolstuff {
-
-    
+export class schoolstuff {
+    "diversity_score": string
+    "enrollment":string
+    "ethnicity":string
+    "grade":string
+    "key":string
+    "month":string
+    "school":string
+    "system":string
+    "year":string
 }
